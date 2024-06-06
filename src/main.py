b6734ac715ce105b8ba163b2f3fb9bedb584c7ee
BukvicNikola <nikibuki2006@gmail.com>
2024-06-06 10:30:20 +0200

    #2 Zahtev za pretragu knjige ispunjen

--- src/main.py
+++ src/main.py
@@ -20,41 +20,8 @@ if unos =="1":
         for object in library.lista_objekata:
             file.write(object.display_info() + "\n")
 
-elif unos == "2":
-    print("Napisi informacije o knjizi koju zelite da izbrisete.")
-    naziv = input("Naziv: ")
-    autor = input("Autor: ")
-    god_izdanja = input("Godina izdanja: ")
-    zanr = input("Zanr: ")
-    book = Book(naziv, autor, god_izdanja, zanr)
-    with open("fajl.txt", "r") as file:
-        lines = file.readlines()
-    with open("fajl.txt", "w") as file:
-        for line in lines:
-            if line.strip() != book.display_info().strip():
-                file.write(line)
-    
-elif unos == "3":
-    print("Napisi informacije o knjizi koju zelite da izmenite.")
-    naziv = input("Naziv: ")
-    autor = input("Autor: ")
-    god_izdanja = input("Godina izdanja: ")
-    zanr = input("Zanr: ")
-    book = Book(naziv, autor, god_izdanja, zanr)
 
-    print("Napisi informacije o izmenjenoj knjizi.")
-    naziv1 = input("Naziv: ")
-    autor1 = input("Autor: ")
-    god_izdanja1 = input("Godina izdanja: ")
-    zanr1 = input("Zanr: ")
-    book1 = Book(naziv1, autor1, god_izdanja1, zanr1)
-
-    with open("fajl.txt", "r") as file:
-        lines = file.readlines()
     
-    lines = [book1.display_info() + '\n' if book.display_info() in line else line for line in lines]
-    with open("fajl.txt", "w") as file:
-        file.writelines(lines)
 
 elif unos == "4":
     kriterjum = input("Po cemu zelite da pretrazujete knjige: ")
